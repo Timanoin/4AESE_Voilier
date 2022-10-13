24e0ca6569777cc5c9536d5024ca5d7406110405
Arthur Gautheron <gauthero@etud.insa-toulouse.fr>
2022-10-13 11:10:16 +0200

    Organisation dossiers pour le projet

---
+++ F1_Voiles_PWM/Sources/main.c
@@ -0,0 +1,21 @@
+/*
+*	Title       : main.c
+* Author      : Arthur Gautheron, Olivier Lagrost, Kenza Bouzergan, Tran Kien Trunh
+* Date        : 13/10/2022
+* Description : Le programme principal du projet Voilier.
+*/
+
+#include "stm32f10x.h"
+
+int main(void)
+{
+	// Déclaration des structures
+	
+	// Initialisation des périphériques
+	
+	// Boucle infinie
+	do
+	{
+		
+	}while(1);
+}
